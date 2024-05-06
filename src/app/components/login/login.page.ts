8b8649210426f485a021d59e92cbcd43cbf9d59c
erickalonso98 <erick.edjaa@gmail.com>
2024-05-05 22:44:38 -0600

    se logro guardar el token y el identity en el localstorage del navegador

--- src/app/components/login/login.page.ts
+++ src/app/components/login/login.page.ts
@@ -14,12 +14,15 @@ export class LoginPage implements OnInit {
   public user:User;
   private _userService = inject(UsersService);
   public status:string;
+  public token:string;
+  public identity:any;
 
   constructor(
     private _router:Router
   ) {
-    this.user = new User(0,'','','','','','',[]);
+    this.user = new User(1,'','','','','','',[]);
     this.status = '';
+    this.token = '';
    }
 
   ngOnInit() {
@@ -31,9 +34,19 @@ export class LoginPage implements OnInit {
     console.log("password:",this.user.password);
 
     this._userService.login(this.user.email,this.user.password).subscribe(
-      (response) => {
+      (response:any) => {
         if(response){
-          console.log(response);
+         console.log(response);
+
+         this.token = response.token;
+         this.identity = response.identity;
+
+         console.log(this.token);
+         console.log(this.identity);
+
+         localStorage.setItem('identity',JSON.stringify(this.identity));
+         localStorage.setItem('token',this.token);
+
           this.status = 'success';
           form.reset();
         }else{
